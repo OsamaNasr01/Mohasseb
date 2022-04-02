417e40336faf6dc8f1de9d10903a2181c4317e6f
Osama <osama.nasr.01@gmail.com>
2022-04-02 10:47:09 +0200

    add table to create view for purchase invoice

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -41,20 +41,43 @@
                 <label for="project_name">Project Name</label>
                 <input class="form-control" type="text" name="project_name">
             </div>
-            @foreach ($items as $item)
-            <div>
-                @php
-                    $total = ($item->product->prices->p_price) * ($item->no);
-                    $subtotal += $total;
-                    $index += 1; 
-                @endphp
-                <div>
-                    <p>
-                        {{ $index }} |  {{ $item->product->name }} | {{ $item->no }} | {{ $item->product->prices->p_price }} | {{ $total }}
-                    </p>
-                </div>
+            <div class="pt-10">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">No</th>
+                            <th scope="col">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($items as $item)
+                            @php
+                                $total = ($item->product->prices->p_price) * ($item->no);
+                                $subtotal += $total;
+                                $index += 1; 
+                                $product = $item -> product;
+                            @endphp
+                            <tr>
+                                <th scope="row">{{ $index }}</th>
+                                <td><a href="{{ ROUTE('product.show', $product) }}">{{ $item->product->name }}</a></td>
+                                <td>{{ $item->product->prices->p_price }}</td>
+                                <td>{{ $item->no }}</td>
+                                <td>{{ $total }}</td>
+                            </tr>
+                        @endforeach
+                        <tr>
+                            <th scope="row">#</th>
+                            <th></th>
+                            <th></th>
+                            <th>Invoice value</th>
+                            <th>{{ $subtotal }}</th>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            @endforeach
             <div class="form-group">
                 <label for="subtotal">Total value of invoice</label>
                 <input class="form-control" type="text" name="subtotal" value="{{ $subtotal }}">
